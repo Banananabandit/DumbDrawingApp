14ad63a9fe2115350f8a113c4c805c0e63509e1a
Pavel Cistjakov <pavelcistjakov@gmail.com>
2022-07-15 06:57:10 +0100

    Implementing drawing functionality the custom view

--- app/src/main/java/android/banananabandit/dumbdrawingapp/DrawingView.kt
+++ app/src/main/java/android/banananabandit/dumbdrawingapp/DrawingView.kt
@@ -1,8 +1,10 @@
 package android.banananabandit.dumbdrawingapp
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 class DrawingView(context : Context, attrs : AttributeSet) : View(context, attrs) {
@@ -29,11 +31,60 @@ class DrawingView(context : Context, attrs : AttributeSet) : View(context, attrs
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
         mBrushSize = 20.toFloat()
 
+    }
 
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        // this is where it was touched
+        val touchX = event?.x
+        val touchY = event?.y
+
+        // during our even we can have multiple actions that we need to intercept
+        when(event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                mDrawPath!!.color = color
+                mDrawPath!!.brushThickness = mBrushSize
+                mDrawPath!!.reset()
+                mDrawPath!!.moveTo(touchX!!, touchY!!)
+
+            }
+            MotionEvent.ACTION_MOVE -> {
+                mDrawPath!!.lineTo(touchX!!, touchY!!)
+            }
+
+            MotionEvent.ACTION_UP -> {
+                mDrawPath = CustomPath(color, mBrushSize)
+            }
+            else -> return false
+        }
+
+        invalidate()
+
+        return true
+
+    }
+
+    //This method will trigger everytime the view is inflated
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitMap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitMap!!)
+    }
+
+    override fun onDraw(canvas: Canvas) { //originally this override will have canvas var that can be variabale, but we dont want that
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitMap!!, 0f, 0f, mCanvasPaint)
+        if (!mDrawPath!!.isEmpty) {
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
     }
 
 
     internal inner class CustomPath(var color : Int, var brushThickness : Float) : Path() {
-
     }
+
+
+
+
 }
